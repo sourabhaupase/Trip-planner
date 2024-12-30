0cd75154646713532f23079501fd8a081e1f221b
sourabhaupase <sourabhaupase2810@gmail.com>
2024-12-30 10:27:11 +0530

    set up route dom and added sign in page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+function SignIn() {
+  return <div>Sign in page</div>;
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
